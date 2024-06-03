62a01884b54d4a8a59b400fef41a763b92248c9a
abhishek kumar <abhisingh2084@gmail.com>
2024-06-03 17:25:31 +0530

    Add project files.

---
+++ AspCoreWebAPICRUD/Controllers/StudentAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreWebAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        
+    }
+}
