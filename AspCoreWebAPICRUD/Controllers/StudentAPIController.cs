947a17860929f4a1d98710441d466a9335ba8bbb
abhishek kumar <abhisingh2084@gmail.com>
2024-06-03 18:26:54 +0530

    Added CRUD Operations

--- AspCoreWebAPICRUD/Controllers/StudentAPIController.cs
+++ AspCoreWebAPICRUD/Controllers/StudentAPIController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AspCoreWebAPICRUD.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspCoreWebAPICRUD.Controllers
 {
@@ -7,6 +9,39 @@ namespace AspCoreWebAPICRUD.Controllers
     [ApiController]
     public class StudentAPIController : ControllerBase
     {
-        
+        private readonly CodeFirstDbContext context;
+
+        public StudentAPIController(CodeFirstDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudent()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentbyId(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await context.Students.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
     }
 }
